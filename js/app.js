0946271dede9715244696c57b6864fd1d39bc675
Lawrence Fitzgerald <lfitzmail@gmail.com>
2017-06-14 18:20:40 -0400

    File updates.
    Added .css file for the self-made ring(will not be used).  Also added two ring images to be used as the background for the actual fight.  Several modifications to the remaining files.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
+// $(document).ready(function() {
+$(".match").hide();
+// });
+
 $(document).ready(function() {
   $("options :button").click(function() {
-    $("titleScreen").html(".match");
+    $(".titleScreen").hide(3000);
   });
 });
+// var a = jQuery.Event("keypress")
 
-var a = jQuery.Event("keypress")
+//
+//
+// function SpriteSheet(path, frameWidth, frameHeight) {
+//
+//    var image = new Image();
+//    var framesPerRow;
+//
+//    // calculate the number of frames in a row after the image loads
+//    var self = this;
+//    image.onload = function() {
+//       framesPerRow = Math.floor(image.width / frameWidth);
+//    };
+//
+//    image.src = path;
+// }
